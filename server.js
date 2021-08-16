9fa9661d892807ce4444dfe449b3cfca652c75ac
shannontorcato <shannon.torcato1234@gmail.com>
2021-08-16 14:49:33 +0530

    added a header and nav-bar

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const fileupload = require('express-upload');
+
+let initial_path = path.join(__dirname, "path");
+
+const app = express();
+app.use(express.static(initial_path));
+app.use(fileupload());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, "home.html"));
+})
+
+app.listen("3000", () =>{
+    console.log('listening.....');
+})
